4ecafc135a65d703f3de18cfb1290250db4421a8
Preet Singh <preetsingh3001@gmail.com>
2023-04-01 13:23:02 +0530

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:show_me_the_gains/firebase_options.dart';
 import 'package:show_me_the_gains/router/router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -12,12 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weight Lifting Logger',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: onGenerateRoute,      
+      onGenerateRoute: onGenerateRoute,
     );
   }
 }
-
